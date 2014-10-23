8d9cb6cccd7ee0d4eeaa33d6be3e1807d5672db6
Jem Mawson <jem.mawson@gmail.com>
2014-10-23 11:37:37 +0800

    social on scala futures post

--- js/social.js
+++ js/social.js
@@ -8,3 +8,17 @@
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+// twitter
+window.twttr = (function (d, s, id) {
+    var t, js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    return window.twttr || (t = {_e: [], ready: function (f) {
+        t._e.push(f)
+    }})
+}(document, "script", "twitter-wjs"));
